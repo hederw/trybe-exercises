767abf5a9bfd1d86a0d250d463573ebb9adc1830
Heder Waine <hederw@hotmail.com>
2021-04-26 16:01:16 -0300

    Exercicio 1

---
+++ Fundamentos_do_Desenvolvimento_Web/BLOCO_4/scripts.js
@@ -0,0 +1,12 @@
+let n = 5;
+let simbolo = '*';
+let linha = '';
+
+for(line = 0; line < n; line +=1){
+    linha += simbolo;
+    
+}
+
+for(let colun = 0; colun < n; colun +=1){
+    console.log(linha);
+}
