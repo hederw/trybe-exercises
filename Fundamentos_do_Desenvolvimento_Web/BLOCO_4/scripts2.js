e21fdd47c28c95b3e31cf86d3cc56f8de93cb0f7
Heder Waine <hederw@hotmail.com>
2021-04-26 17:43:21 -0300

    Exercicio 3

--- Fundamentos_do_Desenvolvimento_Web/BLOCO_4/scripts2.js
+++ Fundamentos_do_Desenvolvimento_Web/BLOCO_4/scripts2.js
@@ -2,7 +2,7 @@ let n = 5;
 let simbolo = '*';
 let linha = '';
 
-for(line = 0; line < n; line +=1){
+for(line = 0; line <= n; line +=1){
 
     console.log(linha);//primeiro imprime
     linha += simbolo;//depois soma
