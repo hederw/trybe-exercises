bb129dada59c2a5e5f233f5f68b9029b84656aa2
Heder Waine <hederw@hotmail.com>
2021-04-26 08:53:40 -0300

    Feito exercicio 3

---
+++ Fundamentos_do_Desenvolvimento_Web/BLOCO_4/DIA_2/scripts3.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let media =0;
+
+for( let index = 0; index < numbers.length; index +=1){
+    media += numbers[index]; 
+    
+}
+
+console.log(media / 10);
